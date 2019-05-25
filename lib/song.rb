fc5d262743c1176c9abd09704291946f17ba80fd
Denali Balser <denali.balser167@gmail.com>
2019-05-25 02:10:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,8 +34,8 @@ class Song
   end
 
   def self.find_or_create_by_name(find_this_song)
-    self.find_by_name(name) || self.create_by_name(name)
-    
+    self.find_by_name(find_this_song) || self.create_by_name(find_this_song)
+
   end
 
   def self.alphabetical()
